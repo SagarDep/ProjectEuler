810d98c8171394227f3d0096367f5dc20447d321
Pagge <Patrick.Ivarsson@gmail.com>
2012-08-30 23:16:25 +0200

    Problem 31 solved

---
+++ src/Problem31to40/Problem32.java
@@ -0,0 +1,20 @@
+package Problem31to40;
+
+public class Problem32 {
+
+	public Problem32(){
+		solve();
+	}
+	
+	private int solve() {
+		
+		
+		
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		new Problem32();
+	}
+
+}
