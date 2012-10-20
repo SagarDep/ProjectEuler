d4d5c225edb967cc4adb66ac42e60ae02b8482e4
Pagge <Patrick.Ivarsson@gmail.com>
2012-10-20 22:43:13 +0200

    Problem 35 done, pretty crappy solution tho

--- src/Library/Primes.java
+++ src/Library/Primes.java
@@ -1,5 +1,6 @@
 package library;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Primes {
@@ -31,5 +32,27 @@ public class Primes {
 		}
 		
 		return sum;
+	}
+	
+	
+	public static ArrayList<Integer> listOfPrimes(int n) {
+		boolean[] array = new boolean[n+1];
+		Arrays.fill(array, true);
+		for (long i = 2; i * i < array.length; i++) {
+			if(array[(int) i]){
+				long j = i * i;
+				while(j < array.length) {
+					array[(int) j] = false;
+					j += i;
+				}
+			}
+		}
+		
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		for (int i = 2; i < array.length; i++) {
+			if(array[i])
+				primes.add(i);
+		}
+		return primes;
 	}
 }
